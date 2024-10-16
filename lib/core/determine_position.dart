59f96652199a4fabaebf90bb7928bbd76ec00fad
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-16 21:38:56 +0300

    add `determinePosition` and `getLocationName`

---
+++ lib/core/determine_position.dart
@@ -0,0 +1,42 @@
+import 'package:geocoding/geocoding.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:latlong2/latlong.dart';
+
+Future<Position> determinePosition() async {
+  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+  if (!serviceEnabled) {
+    return Future.error('Location services are disabled.');
+  }
+
+  LocationPermission permission = await Geolocator.checkPermission();
+  if (permission == LocationPermission.denied) {
+    permission = await Geolocator.requestPermission();
+    if (permission == LocationPermission.denied) {
+      return Future.error('Location permissions are denied');
+    }
+  }
+
+  if (permission == LocationPermission.deniedForever) {
+    return Future.error(
+        'Location permissions are permanently denied, we cannot request permissions.');
+  }
+
+  return await Geolocator.getCurrentPosition();
+}
+
+Future<String> getLocationName(LatLng position) async {
+  // 30.99674449616613,30.56313391804571
+
+  final placemarks = await placemarkFromCoordinates(
+    position.latitude,
+    position.longitude,
+  );
+  if (placemarks.isNotEmpty) {
+    final place = placemarks.first;
+    if (place.locality != null) {
+      return '${place.locality}, ${place.administrativeArea}, ${place.country}';
+    }
+    return ' ${place.administrativeArea}, ${place.country}';
+  }
+  return 'Unknown Location';
+}
