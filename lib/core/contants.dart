26ea2a31968e5031c382cc911f570bf4dcfd0830
Mohamed magdy <mohammedmego15@gmail.com>
2024-10-17 02:33:17 +0300

    added file for constants in app

---
+++ lib/core/contants.dart
@@ -0,0 +1,5 @@
+const String historyKey = "historyKey";
+const String orsApiKey =
+    '5b3ce3597851110001cf6248beb9203a7f984873a325612ddaa21c24';
+const String orsApiUrl =
+    'https://api.openrouteservice.org/v2/directions/foot-walking';
